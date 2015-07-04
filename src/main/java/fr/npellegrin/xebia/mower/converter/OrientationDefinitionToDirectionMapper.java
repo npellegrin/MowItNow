302336216c9f802be6fe53b1f7d9723f4309e514
Nicolas Pellegrin <npellegrin.dev@gmail.com>
2015-07-04 14:03:52 +0200

    Mapper: convert OrientationDefinition to Direction

--- src/main/java/fr/npellegrin/xebia/mower/converter/OrientationDefinitionToDirectionMapper.java
+++ src/main/java/fr/npellegrin/xebia/mower/converter/OrientationDefinitionToDirectionMapper.java
@@ -1,14 +1,28 @@
 package fr.npellegrin.xebia.mower.converter;
 
 import fr.npellegrin.xebia.mower.orientation.Direction;
+import fr.npellegrin.xebia.mower.orientation.EastDirection;
+import fr.npellegrin.xebia.mower.orientation.NorthDirection;
+import fr.npellegrin.xebia.mower.orientation.SouthDirection;
+import fr.npellegrin.xebia.mower.orientation.WestDirection;
 import fr.npellegrin.xebia.mower.parser.model.OrientationDefinition;
 
 public class OrientationDefinitionToDirectionMapper implements Mapper<OrientationDefinition, Direction> {
 
 	@Override
 	public Direction map(OrientationDefinition input) {
-		// TODO Auto-generated method stub
-		return null;
+		switch (input) {
+		case N:
+			return NorthDirection.getInstance();
+		case E:
+			return EastDirection.getInstance();
+		case O:
+			return WestDirection.getInstance();
+		case S:
+			return SouthDirection.getInstance();
+		default:
+			return null;
+		}
 	}
 
 }

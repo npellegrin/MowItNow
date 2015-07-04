cd8b566034695fba7210f9098812cdf9d88612ab
Nicolas Pellegrin <npellegrin.dev@gmail.com>
2015-07-04 13:57:49 +0200

    Mapper test: convert OrientationDefinition to Direction

---
+++ src/main/java/fr/npellegrin/xebia/mower/converter/OrientationDefinitionToDirectionMapper.java
@@ -0,0 +1,14 @@
+package fr.npellegrin.xebia.mower.converter;
+
+import fr.npellegrin.xebia.mower.orientation.Direction;
+import fr.npellegrin.xebia.mower.parser.model.OrientationDefinition;
+
+public class OrientationDefinitionToDirectionMapper implements Mapper<OrientationDefinition, Direction> {
+
+	@Override
+	public Direction map(OrientationDefinition input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/fr/npellegrin/xebia/mower/converter/OrientationDefinitionToDirectionMapperTest.java
@@ -0,0 +1,24 @@
+package fr.npellegrin.xebia.mower.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import fr.npellegrin.xebia.mower.parser.model.OrientationDefinition;
+
+public class OrientationDefinitionToDirectionMapperTest {
+
+	@Test
+	public void should_map_parameters() {
+		OrientationDefinitionToDirectionMapper mapper = new OrientationDefinitionToDirectionMapper();
+		assertThat(mapper.map(OrientationDefinition.N).getXVector()).isEqualTo(0);
+		assertThat(mapper.map(OrientationDefinition.N).getYVector()).isEqualTo(1);
+		assertThat(mapper.map(OrientationDefinition.E).getXVector()).isEqualTo(1);
+		assertThat(mapper.map(OrientationDefinition.E).getYVector()).isEqualTo(0);
+		assertThat(mapper.map(OrientationDefinition.S).getXVector()).isEqualTo(0);
+		assertThat(mapper.map(OrientationDefinition.S).getYVector()).isEqualTo(-1);
+		assertThat(mapper.map(OrientationDefinition.O).getXVector()).isEqualTo(-1);
+		assertThat(mapper.map(OrientationDefinition.O).getYVector()).isEqualTo(0);
+	}
+
+}

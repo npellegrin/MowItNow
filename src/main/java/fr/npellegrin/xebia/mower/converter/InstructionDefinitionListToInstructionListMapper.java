96bb622241cbfd3ae90c4c642236831a879a7f69
Nicolas Pellegrin <npellegrin.dev@gmail.com>
2015-07-05 19:21:30 +0200

    Refactoring: internal mappers can be static

--- src/main/java/fr/npellegrin/xebia/mower/converter/InstructionDefinitionListToInstructionListMapper.java
+++ src/main/java/fr/npellegrin/xebia/mower/converter/InstructionDefinitionListToInstructionListMapper.java
@@ -11,18 +11,14 @@ import fr.npellegrin.xebia.mower.parser.model.InstructionDefinition;
  */
 public class InstructionDefinitionListToInstructionListMapper
 		implements ListMapper<InstructionDefinition, Instruction> {
-	private final InstructionDefinitionToInstructionMapper instructionMapper;
-
-	public InstructionDefinitionListToInstructionListMapper() {
-		instructionMapper = new InstructionDefinitionToInstructionMapper();
-	}
+	private static final InstructionDefinitionToInstructionMapper MAPPER = new InstructionDefinitionToInstructionMapper();
 
 	@Override
 	public List<Instruction> map(final List<InstructionDefinition> input) {
 		// FIXME: use Java 8 stream() to simplify conversion
 		final List<Instruction> result = new ArrayList<Instruction>();
 		for (final InstructionDefinition instruction : input) {
-			result.add(instructionMapper.map(instruction));
+			result.add(MAPPER.map(instruction));
 		}
 		return result;
 	}
--- src/main/java/fr/npellegrin/xebia/mower/converter/PositionDefinitionToOrientationMapper.java
+++ src/main/java/fr/npellegrin/xebia/mower/converter/PositionDefinitionToOrientationMapper.java
@@ -7,15 +7,11 @@ import fr.npellegrin.xebia.mower.parser.model.PositionDefinition;
  * MAp a position definition to an orientation.
  */
 public class PositionDefinitionToOrientationMapper implements Mapper<PositionDefinition, Orientation> {
-	private final OrientationDefinitionToDirectionMapper orientationDefinitionToDirectionMapper;
-
-	public PositionDefinitionToOrientationMapper() {
-		orientationDefinitionToDirectionMapper = new OrientationDefinitionToDirectionMapper();
-	}
+	private static final OrientationDefinitionToDirectionMapper MAPPER = new OrientationDefinitionToDirectionMapper();
 
 	@Override
 	public Orientation map(final PositionDefinition input) {
-		return new Orientation(orientationDefinitionToDirectionMapper.map(input.getOrientation()));
+		return new Orientation(MAPPER.map(input.getOrientation()));
 	}
 
 }
